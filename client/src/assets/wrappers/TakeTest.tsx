437692abbeaf0c528470e47cdd769b187c5828d8
Moshood <moshood.yemi10@gmail.com>
2022-08-11 03:52:56 +0800

    take test page design initiated

--- client/src/assets/wrappers/TakeTest.tsx
+++ client/src/assets/wrappers/TakeTest.tsx
@@ -3,6 +3,12 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   background-color: var(--primary-100);
   min-height: 100vh;
+
+  .take-test-container {
+    width: 90%;
+    margin: 0 auto;
+  }
+
   .img-div {
     width: 100%;
     height: 8vh;
@@ -13,6 +19,29 @@ const Wrapper = styled.div`
 
   .img-div img {
     width: 100px;
+  }
+
+  .take-test-welcome {
+    background-color: var(--primary-200);
+    color: var(--white);
+    padding: .5em;
+    font-size: 1.2rem;
+    border-radius: 10px;
+    text-align: center;
+    margin-bottom: 1em;
+  }
+
+  .quiz-details {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1em;
+  }
+
+  .quiz-details-title {
+    border-bottom: solid 1px var(--primary-200);
+    display: flex;
+    font-size: 0.9rem;
+    margin-bottom: .5em;
   }
 `;
 export default Wrapper;
--- client/src/pages/TakeTest.tsx
+++ client/src/pages/TakeTest.tsx
@@ -12,20 +12,49 @@ const TakeTest = () => {
   useEffect(() => {
     getParticipantQuizInfo();
   }, []);
+
   return (
     <Wrapper>
-      {isLoading ? <Loading /> :
-        <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="take-test-container">
           <div className="img-div">
             <Logo />
           </div>
+          <div className="take-test-welcome">Welcome to today's quiz, Moshood.</div>
           <div>
-            <div></div>
+            <div className="quiz-details">
+              <div>
+                <span className="quiz-details-title">Student name:</span>
+                <h4>Steven Wagner</h4>
+              </div>
+              <div>
+                <span className="quiz-details-title">identifier:</span>
+                <h4>2003012013</h4>
+              </div>
+              <div>
+                <span className="quiz-details-title">Quiz title</span>
+                <h4>Biology 2021/2022</h4>
+              </div>
+              <div>
+                <span className="quiz-details-title">Duration</span>
+                <h4>20 mins</h4>
+              </div>
+              <div>
+                <span className="quiz-details-title">Start Date</span>
+                <h4>Moshood Abdullahi</h4>
+              </div>
+              <div>
+                <span className="quiz-details-title">End Date</span>
+                <h4>Moshood Abdullahi</h4>
+              </div>
+            </div>
             <div></div>
           </div>
           Take Test
-        </>
-      }
+        </div>
+      )}
     </Wrapper>
   );
 };
