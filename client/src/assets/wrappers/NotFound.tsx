98eb85acfcdc85612cbf390edfc81060670c1ab4
Moshood <moshood.yemi10@gmail.com>
2022-10-06 05:06:48 -0400

    not found page added

---
+++ client/src/assets/wrappers/NotFound.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Wrapper = styled.main`
+  text-align: center;
+  padding-top: 4em;
+  width: 90%;
+  margin: 0 auto;
+  img {
+    max-width: 600px;
+    display: block;
+    margin-bottom: 2rem;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h3 {
+    margin-bottom: 0.5rem;
+  }
+  p {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+    color: var(--grey-500);
+  }
+  a {
+    color: var(--primary-500);
+    text-decoration: underline;
+    text-transform: capitalize;
+  }
+`;
+
+export default Wrapper;
--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/NotFound";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-    </div>
-  )
-}
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Ohh! page not found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default NotFound
+export default NotFound;
